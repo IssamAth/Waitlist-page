de2de2b2cb8f851e230b716275bc092678a789a9
Issame Athamena <samo.alger@gmail.com>
2022-10-27 22:14:07 +0100

    tablet design almost done, but there few more things to fix like the margin of the container problem

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -4,7 +4,7 @@ import './content.css'
 const Content = () => {
   return (
     <section className='content'>
-      <h4>We are now allowing early-access for users. <a className='learn'>Learn more</a></h4>
+      <h4>We are now allowing early-access for users. <a className='learn'>Learn more.</a></h4>
       <h1>Build a highly engaged community with no effort.</h1>
       <p>Commune offers the tools you need to build a highly engaged community with little to no effort. Simply setup your Commune workspace, and manage everything from members to content from one central dashboard.</p>
     </section>
