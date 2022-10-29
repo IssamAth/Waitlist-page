2f19c6dcd8e8edfd8846dab38a8ee952793df785
Issame Athamena <samo.alger@gmail.com>
2022-10-29 22:19:15 +0100

    fixed the problem with the navigation bar not showing in desktop screens by adding that condition and making new class only for desktops

--- src/components/navbarlogo/Navbarlogo.jsx
+++ src/components/navbarlogo/Navbarlogo.jsx
@@ -1,42 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './navbarlogo.css'
 import logo from '../../assets/Logo.svg'
 import {GiHamburgerMenu} from 'react-icons/gi'
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
 
+const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
 
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
+const Navbarlogo = () => {
+  const [displayTopNav, setDisplayTopNav] = useState(false)
+  console.log(vw);
+
+  function testing(displayTopNav){
+    if ((displayTopNav  === true) && (vw < 1024)) {
+      return "topnav";
+    }  if ((displayTopNav === false) && (vw < 1024)) {
+      return "topnavhide";
+    } else if ((displayTopNav === true) && (vw >= 1024)) {
+      return "newclass";
+    }
+    
   }
-}
 
-
-const navbarlogo = () => {
   return (
     <div id='navbarlogo'>
       <div className="logo-menu">
         <div className='logo'>
           <img src={logo} alt="" /> 
         </div>
-        <a href="#" class="icon" onClick={myFunction}>
+        <a href="#" class="icon" onClick={() => setDisplayTopNav(!displayTopNav)}>
             <GiHamburgerMenu size = '28'/>
         </a>
       </div>
-      <nav class="topnav">
-        <ul id="myLinks">
-          <li><a href="#">Features</a></li>
-          <li><a href="#">Affiliates</a></li>
-          <li><a href="#">Pricing</a></li>
-          <li><a href="#">Communities</a></li>
-          <li><a href="#">Join Waitlist</a></li>
-        </ul>
-      </nav>
+        <nav className={testing(displayTopNav)}>
+          <ul className="mylinks">
+            <li><a href="#">Features</a></li>
+            <li><a href="#">Affiliates</a></li>
+            <li><a href="#">Pricing</a></li>
+            <li><a href="#">Communities</a></li>
+            <li><a href="#">Join Waitlist</a></li>
+          </ul>
+        </nav>
     </div>
   )       
 }
 
-export default navbarlogo
+export default Navbarlogo
