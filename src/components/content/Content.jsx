5fd0318a007330d826b8b78ef100f056f8ed3528
Issame Athamena <samo.alger@gmail.com>
2022-10-30 20:58:22 +0100

    pushing before starting to fix things and test

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './content.css'
+import Illustration from '../../assets/Illustration.png'
 
 const Content = () => {
   return (
@@ -7,6 +8,9 @@ const Content = () => {
       <h4>We are now allowing early-access for users. <a className='learn'>Learn more.</a></h4>
       <h1>Build a highly engaged community with no effort.</h1>
       <p>Commune offers the tools you need to build a highly engaged community with little to no effort. Simply setup your Commune workspace, and manage everything from members to content from one central dashboard.</p>
+      <div className="image">
+        <img src={Illustration} alt="illustration" />
+      </div>
     </section>
   )
 }
