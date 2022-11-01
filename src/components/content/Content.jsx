2bab9d1ff0407a81ded29fa7f9c00db88a89cf08
Issame Athamena <samo.alger@gmail.com>
2022-11-01 01:32:52 +0100

    desktop view ready but there are other things to fix such as small desktop view and also the margin on tabs and small laptops and content texts - Ps: you shoud always work on smaller screens then move up

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -13,9 +13,11 @@ const Content = () => {
   }
   return (
     <section className='content'>
-      <h4>We are now allowing early-access for users. <a className='learn'>Learn more.</a></h4>
-      <h1>Build a highly engaged community with no effort.</h1>
-      <p>Commune offers the tools you need to build a highly engaged community with little to no effort. Simply setup your Commune workspace, and manage everything from members to content from one central dashboard.</p>
+      <div className="h4-h1">
+        <h4>We are now allowing early-access for users. <a className='learn'>Learn more.</a></h4>
+        <h1>Build a highly engaged community with no effort.</h1>
+        <p>Commune offers the tools you need to build a highly engaged community with little to no effort. Simply setup your Commune workspace, and manage everything from members to content from one central dashboard.</p>
+      </div>
       <div className="image">
         <img src={Illustration} alt="illustration" />
       </div>
